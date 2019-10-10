35405cdbb22171b4c190ea531d8994cd62c4893b
Victoria Sayevich <VictoriaSayevich@gmail.com>
2019-10-10 22:57:06 +0200

    feat: complete task guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,32 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.start = 0;
+        this.end = 0;
+        this.arr_to_search = [];
+        this.middle = 0;
+    }
+        
     setRange(min, max) {
-
+        this.start = min;
+        this.end = max;
+        for (let i = min; i <= max; i++) {
+            this.arr_to_search.push(i);
+        }
+        this.middle = Math.round((this.start + this.end)/2);
     }
-
+        
     guess() {
-
+        return this.arr_to_search[this.middle];
     }
-
+        
     lower() {
-
+        this.end = this.middle;
+        this.middle = Math.round((this.start + this.end)/2);
     }
-
+        
     greater() {
-
+        this.start = this.middle;
+        this.middle = Math.round((this.start + this.end)/2);
     }
 }
 
